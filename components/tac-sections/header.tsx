9c981a7cdb98ef55fa82c571121502b6156613d8
misaw-kun <w.akram.1912@protonmail.com>
2024-08-27 14:02:58 +0530

    link header logo to homepage

--- components/tac-sections/header.tsx
+++ components/tac-sections/header.tsx
@@ -2,12 +2,15 @@ import Image from "next/image";
 import React from "react";
 import CalPopup from "../ui/cal-popup";
 import Logo from "@/app/assets/logo.svg";
+import Link from "next/link";
 
 function TacHeader() {
   return (
     <header className="py-8 lg:py-12 w-full">
       <div className="flex items-center justify-between">
-        <Image src={Logo} alt="brand-logo" />
+        <Link href="/">
+          <Image src={Logo} alt="brand-logo" />
+        </Link>
         <CalPopup btnText="Contact Us" isHeader />
       </div>
     </header>
